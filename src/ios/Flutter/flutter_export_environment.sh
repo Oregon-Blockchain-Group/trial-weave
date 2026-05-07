c5e94f5754b114800c233d2f975657e58e1ed081
Justin hatch <115518370+jhatch3@users.noreply.github.com>
2026-05-06 17:35:37 -0700

    [TASK] - Rough Draft MVP All Done, All Connected to Supabase

--- src/ios/Flutter/flutter_export_environment.sh
+++ src/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Justin\fvm\versions\3.41.5"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Justin\trial-weave"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Justin\trial-weave\src"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
